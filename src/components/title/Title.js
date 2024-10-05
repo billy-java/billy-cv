b85c8cc7f792b0a855e64d270197465b4bfe99ae
Billy <95180027+billy-java@users.noreply.github.com>
2024-10-05 18:03:21 +0200

    last commit

--- src/components/title/Title.js
+++ src/components/title/Title.js
@@ -1,14 +1,15 @@
-import React from "react";
-import "./Title.css";
+import React from 'react';
+import './Title.css';
 
 export default function Title(props) {
   return (
     <div className="text-center">
       <h2 className="my_color">{props.title}</h2>
       <p className="my_p">
-        Tel : +49 176 34 36 49 86
+        Tel : <a href="tel:+4917634364986">+49 176 34 36 49 86</a>
         <br />
-        Email : billybabou007@gmail.com
+        Email :{' '}
+        <a href="mailto:billybabou007@gmail.com">billybabou007@gmail.com</a>
       </p>
     </div>
   );
